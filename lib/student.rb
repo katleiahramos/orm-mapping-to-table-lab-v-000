afdaa7862dbced42e89358a603935c540b1550c9
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-10 17:00:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,4 +11,15 @@ class Student
     @id = id
   end
 
+  def self.create_table 
+    sql =  <<-SQL 
+        CREATE TABLE IF NOT EXISTS songs (
+          id INTEGER PRIMARY KEY, 
+          name TEXT, 
+          grade INTEGER
+          )
+          SQL
+    DB[:conn].execute(sql) 
+  end
+
 end
