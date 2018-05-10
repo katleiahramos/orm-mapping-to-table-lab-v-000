98a5e75d47eebee2c4507f3576efe78df7b0f94e
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-10 17:06:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,11 +30,12 @@ class Student
   end
 
   def save
+    binding.pry
     sql = <<-SQL
-      INSERT INTO students (name,grade)
+      INSERT INTO students (name,grade) 
       VALUES (?,?)
       SQL
       DB[:conn].execute(sql,self.name, self.grade)
-  end
+  end 
 
 end
